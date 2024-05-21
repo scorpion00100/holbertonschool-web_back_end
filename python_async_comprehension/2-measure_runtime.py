9ea9cae2b5411b63ea18795cd86a933294b32b17
scorpion00100 <danbetheliryivuze@gmail.com>
2024-05-21 10:21:21 +0200

    adding all tasks after test locally

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Task 2. Measure the runtime"""
+
+import asyncio
+import time
+from typing import List
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    Measures the total execution time for wait_n(n, max_delay),
+    and returns total_time / n.
+    """
+    start: float = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    elapsed: float = time.perf_counter() - start
+
+    return elapsed / n
