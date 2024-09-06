3982cb47135073ea0dde3d38ec109c7dce5d0032
scorpion00100 <danbetheliryivuze@gmail.com>
2024-09-06 09:53:18 +0200

    try catch it

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,33 +1,34 @@
 #!/usr/bin/env python3
-""" Basic Flask app """
+"""
+Basic word for doc
+"""
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
-app = Flask(__name__)
-babel = Babel(app)
 
-
-class Config:
-    """ Config """
-    LANGUAGES = ['en', 'fr']
+class Config():
+    """ class Config """
+    LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
 
 
-@babel.localeselector
-def get_locale():
-    """ Locale selector """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+@app.route("/")
+def index():
+    """ index """
+    return render_template("3-index.html")
 
 
-@app.route('/')
-def root():
-    """ Basic Flask app """
-    return render_template('3-index.html')
+def get_locale() -> str:
+    """ get local languages """
+    return request.accept_languages.best_match(Config.LANGUAGES)
 
+
+babel = Babel(app, locale_selector=get_locale)
 
 if __name__ == "__main__":
     app.run()
