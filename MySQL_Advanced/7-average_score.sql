82c3cc2efc8a9ac41d26883a1f01d534de59fabb
scorpion00100 <danbetheliryivuze@gmail.com>
2024-09-18 13:34:24 +0200

    Script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student.

---
+++ MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,13 @@
+-- Script that creates a stored procedure ComputeAverageScoreForUser
+-- that computes and stores the average score for student.
+-- Create the stored procedure
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+BEGIN
+    UPDATE users
+    SET average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+    WHERE id = user_id;
+END;//
+
+DELIMITER ;
