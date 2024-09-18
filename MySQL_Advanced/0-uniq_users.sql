aa5bf4d27efe9ed844b5990ecf8e46c206675cbc
scorpion00100 <danbetheliryivuze@gmail.com>
2024-09-18 13:12:03 +0200

    SQL script that creates a table users following these requirements

---
+++ MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- Script that creates a table users
+-- Create the table
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
