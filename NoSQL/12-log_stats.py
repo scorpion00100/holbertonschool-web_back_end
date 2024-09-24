bc42ba7ef0f86069aef9c7a47eaa45ea8d6d0469
scorpion00100 <danbetheliryivuze@gmail.com>
2024-09-24 12:22:45 +0200

    try to fix bug

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,24 +1,15 @@
 #!/usr/bin/env python3
-"""Script that provides some stats about Nginx logs stored in MongoDB"""
-
+"""
+a script that provides some stats about a nginx dump logs stored in mongo xxx
+"""
 from pymongo import MongoClient
 
+
 if __name__ == "__main__":
-
     client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_collection = client.logs.nginx
-
-    documents = nginx_collection.count_documents({})
-    print("{} logs".format(documents))
-
-    print("Methods:")
-    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-        print("\tmethod {}: {}".format(
-            method,
-            nginx_collection.count_documents({"method": method})
-        ))
-
-    print("{} status check".format(
-        nginx_collection.count_documents({"$and": [{"path": "/status"},
-                                                   {"method": "GET"}]})
-    ))
+    c = client.logs.nginx
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("{} logs\nMethods:".format(c.count_documents({})))
+    for m in method:
+        print("\tmethod {}: {}".format(m, c.count_documents({"method": m})))
+    print("{} status check".format(c.count_documents({"method": "GET", "path": "/status"})))
