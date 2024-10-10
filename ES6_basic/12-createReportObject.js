080933abb866af67cc1c90b3dc2001c735c8afde
scorpion00100 <danbetheliryivuze@gmail.com>
2024-10-10 19:00:28 +0200

    Let's create a report object

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,8 @@
+export default function createReportObject(employeesList) {
+    return {
+      getNumberOfDepartments() {
+        return Object.keys(employeesList).length;
+      },
+      allEmployees: employeesList,
+    };
+  }
