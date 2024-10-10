896e63fbe168e80b43113009946f663da09d11eb
scorpion00100 <danbetheliryivuze@gmail.com>
2024-10-10 18:14:12 +0200

    try again

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,13 +1,13 @@
 export default function taskBlock(trueOrFalse) {
     const task = false;
     const task2 = true;
-  
+
     if (trueOrFalse) {
-      const task = true;
-      const task2 = false;
-  
-      return [task2, task];
+        const task = true;
+        const task2 = false;
+        
+        return [task2, task];
     }
-  
+
     return [task, task2];
-  }
+}
