82b4e5e48a2743058897b9a3c83af7d84b4f8101
scorpion00100 <danbetheliryivuze@gmail.com>
2024-10-10 18:53:03 +0200

    fixed bug

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,5 +1,3 @@
-import getSumOfHoods from './3-default-parameter.js';
-
-console.log(getSumOfHoods(34));
-console.log(getSumOfHoods(34, 3));
-console.log(getSumOfHoods(34, 3, 4));
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+    return initialNumber + expansion1989 + expansion2019;
+}
