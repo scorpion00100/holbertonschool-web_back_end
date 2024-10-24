e6821f3dcbdd5522137a05a72127304773e4b08a
scorpion00100 <danbetheliryivuze@gmail.com>
2024-10-24 14:24:02 +0200

    Write and export a function named handleProfileSignup.

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,8 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return (Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then(
+    (value) => value.map((result) => ({ status: result.status, value: result.status === 'fulfilled' ? result.value : `${result.reason.name}: ${result.reason.message}` })),
+  ));
+}
